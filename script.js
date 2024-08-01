d52a27cbe162677e9e92381feb25e8e16fdcc89b
Henrique Cunha <henriquecardosocunha@hotmail.com>
2024-08-01 12:18:44 -0300

    Projeto completo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // FORMULARIO
 
-let formElement = document.querySelector('#pop-up__form');
+let formElement = document.querySelector("#pop-up__form");
 
-let nameInput = document.querySelector('.form__edit-name');
-let infoInput = document.querySelector('.form__edit-info');
+let nameInput = document.querySelector(".form__edit-name");
+let infoInput = document.querySelector(".form__edit-info");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -11,55 +11,51 @@ function handleProfileFormSubmit(evt) {
   let nameValue = nameInput.value;
   let infoValue = infoInput.value;
 
-  let profileName = document.querySelector('.profile__info-user');
-  let profileDescription = document.querySelector('.profile__description');
+  let profileName = document.querySelector(".profile__info-user");
+  let profileDescription = document.querySelector(".profile__description");
 
   profileName.textContent = nameValue;
   profileDescription.textContent = infoValue;
 
-  nameInput.value = '';
-  infoInput.value = '';
+  nameInput.value = "";
+  infoInput.value = "";
 
-  popUp.classList.add('disable')
+  popUp.classList.add("disable");
 
-  submitButton.classList.remove('form__edit-button-active');
+  submitButton.classList.remove("form__edit-button-active");
 }
 
-formElement.addEventListener('submit', handleProfileFormSubmit);
-
+formElement.addEventListener("submit", handleProfileFormSubmit);
 
 // BOTAO DE EDITAR ABRIR O FORM
 
-let editButton = document.querySelector('.profile__edit-button');
+let editButton = document.querySelector(".profile__edit-button");
 
-let popUp = document.querySelector('.pop-up')
+let popUp = document.querySelector(".pop-up");
 
-editButton.addEventListener('click', function() {
-  popUp.classList.remove('disable');
+editButton.addEventListener("click", function () {
+  popUp.classList.remove("disable");
 });
-
 
 // BOTAO X DO POP UP FECHAR
 
-let closeButton = document.querySelector('.form__close-button');
+let closeButton = document.querySelector(".form__close-button");
 
-closeButton.addEventListener('click', function() {
-  popUp.classList.add('disable')
-})
-
+closeButton.addEventListener("click", function () {
+  popUp.classList.add("disable");
+});
 
 // BOTAO SUBMIT MUDAR DE CLASSE COM TEXTO ESCRITO
 
-let submitButton = document.querySelector('.form__edit-button');
+let submitButton = document.querySelector(".form__edit-button");
 
 function checkInputs() {
-  if (nameInput.value !== '' && infoInput.value !== '') {
-      submitButton.classList.add('form__edit-button-active');
+  if (nameInput.value !== "" && infoInput.value !== "") {
+    submitButton.classList.add("form__edit-button-active");
   } else {
-      submitButton.classList.remove('form__edit-button-active');
+    submitButton.classList.remove("form__edit-button-active");
   }
 }
 
-nameInput.addEventListener('input', checkInputs);
-infoInput.addEventListener('input', checkInputs);
-
+nameInput.addEventListener("input", checkInputs);
+infoInput.addEventListener("input", checkInputs);
